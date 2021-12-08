8e78f4e1ceba2a4fb949f1d2f1b2025e55fa43e1
unknown <rafo.poghosyan1@gmail.com>
2021-12-08 14:33:09 +0400

    last commitjjhhjdsdahhhvjh

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
 
             <Routes>
-                <Route exact path={'/home'} element={<Home/>} />
+                <Route exact path={'/food'} element={<Home/>} />
                 <Route exact path={'/dashboard'} element={<Dashboard/>}/>
             </Routes>
 
