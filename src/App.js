a8a8c5fd6e12ccb95cbad1701a133ed7a0a4b67b
unknown <rafo.poghosyan1@gmail.com>
2021-12-08 14:56:55 +0400

    last commitjjhhjdsdahhhvjhjj

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import React from 'react';
 import Home from './app/pages/home/Home';
-import {BrowserRouter as Router, Route, Switch}  from "react-router-dom";
+import {Route, Switch}  from "react-router-dom";
 import Dashboard from "./app/pages/dashboard/Dashboard";
 
 function App() {
   return (
-            <Router>
                 <Switch>
                     <Route exact path='/food' component={Home} />
                     <Route exact path='/food/dashboard' component={Dashboard}/>
                 </Switch>
-            </Router>
 
 
   );
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import PassingContentItem from "./app/components/context/AuthContext";
+import {BrowserRouter as Router} from  'react-router-dom';
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <PassingContentItem>
-          <App />
-      </PassingContentItem>
+      <Router basemname={`/${process.env.PUBLIC_URL}`}>
+          <PassingContentItem>
+              <App />
+          </PassingContentItem>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
